0d521498b451b6ca4b3a39457afa898227da25c3
binju <BinjuRai>
2024-06-17 10:30:57 +0545

    add product

--- app/src/main/java/com/example/secondtask/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/secondtask/ui/activity/MainActivity.kt
@@ -1,17 +1,51 @@
-package com.example.secondtask.ui.activity
+package com.example.crud.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.secondtask.R
+import com.example.crud.R
+import com.example.crud.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
+    var auth:FirebaseAuth=FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mainBinding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.ForgetPassword.setOnClickListener{
+            var intent=Intent(this@MainActivity, ForgetActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+        mainBinding.butregister.setOnClickListener {
+            var email:String=mainBinding.editEmail.text.toString()
+            var password:String=mainBinding.editPassword.text.toString()
+
+            auth.createUserWithEmailAndPassword(email,password).
+            addOnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(applicationContext,"Registration Sucessfull",Toast.LENGTH_LONG).show()
+
+                    //navigate to dashboard
+                    var intent=Intent(this@MainActivity, DashboardActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(applicationContext,it.exception?.message,Toast.LENGTH_LONG).show()
+
+                }
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/secondtask/utills/LoadingUtils.kt
+++ app/src/main/java/com/example/secondtask/utills/LoadingUtils.kt
@@ -1,4 +1,21 @@
-package com.example.secondtask.utills
+package com.example.crud.utils
 
-class LoadingUtils {
+import android.app.Activity
+import android.app.AlertDialog
+import com.example.crud.R
+
+class LoadingUtils (val activity: Activity){
+    lateinit var alertDialog: AlertDialog
+
+    fun showLoading(){
+        var dialogView=activity.layoutInflater.inflate(R.layout.loading_diaglog,null)
+        var builder= AlertDialog.Builder(activity)
+        builder.setView(dialogView)
+        builder.setCancelable(false)
+        alertDialog=builder.create()
+        alertDialog.show()
+    }
+    fun dismiss(){
+        alertDialog.dismiss()
+    }
 }
